45781f26b93afeb41828dbfd108304f971fce433
Shubham Saxena <shubham20794@gmail.com>
2024-10-08 19:06:44 +0530

    Add multer error handler

--- node-runtime/src/routes/user.route.js
+++ node-runtime/src/routes/user.route.js
@@ -1,3 +1,4 @@
+import multer from "multer";
 import express from "express";
 
 import {
@@ -15,7 +16,21 @@ const router = express.Router({ mergeParams: true });
 router
   .route("/")
   .get(getAllUser)
-  .post(upload.single("profilePicture"), addUser);
+  .post(function (req, res, next) {
+    upload.single("profilePicture")(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        console.log(err);
+        return res.status(400).send(err);
+      } else if (err) {
+        console.log(err);
+        return res.status(400).send(err);
+      }
+      console.log("=> profile picture uploaded successfully");
+      return next();
+    });
+  }, addUser);
+
+// .post(upload.single("profilePicture"), addUser);
 
 router.route("/:userId").get(getUserById);
 router.route("/:userId/follow").put(followUser);
